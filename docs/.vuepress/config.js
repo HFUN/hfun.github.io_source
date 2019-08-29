a234122b5e3d2423cdcae0a588cc9bca23d52e0e
I333497 <webster.huang@sap.com>
2019-08-29 14:12:27 +0800

    [NEW] init vuepress

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    title: 'HFUN.ME',
+    description: 'Just playing around',
+    themeConfig: {
+        nav: [
+            { text: 'Home', link: '/' },
+            { text: 'Guide', link: '/guide/' },
+            { text: 'External', link: 'https://google.com' },
+        ],
+        sidebar: [
+            ['/page-a', 'Explicit link text'],
+            ['/page-b', 'Explicit link text'],
+            ['/page-c', 'Explicit link text']
+        ]
+    }
+};
---
+++ docs/javascript/web-worker.md
@@ -0,0 +1,5 @@
+# Web Worker
+
+## Chapter 1
+## Chapter 2
+
