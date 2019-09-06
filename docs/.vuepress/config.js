05e348dbdb5af93c3fe30fbc2a85169055daf2e2
I333497 <webster.huang@sap.com>
2019-09-06 16:00:12 +0800

    [NEW] add .travis.yml

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,16 +1,13 @@
 module.exports = {
     title: 'HFUN.ME',
     description: 'Just playing around',
+    base: '/',
     themeConfig: {
         nav: [
             { text: 'Home', link: '/' },
             { text: 'Guide', link: '/guide/' },
             { text: 'External', link: 'https://google.com' },
         ],
-        sidebar: [
-            ['/page-a', 'Explicit link text'],
-            ['/page-b', 'Explicit link text'],
-            ['/page-c', 'Explicit link text']
-        ]
+        sidebar: 'auto'
     }
 };
